660ac2c1e5b90c8977384b5d5bc236e9a4c3faf4
chylex <info@chylex.com>
2016-04-09 15:46:59 +0200

    Add essential classes - entry point and the browser Form

---
+++ Forms/FormBrowser.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace TweetDick.Forms{
+    public partial class FormBrowser : Form{
+        private readonly ChromiumWebBrowser browser;
+
+        public FormBrowser(){
+            InitializeComponent();
+
+            browser = new ChromiumWebBrowser("https://tweetdeck.twitter.com/");
+            Controls.Add(browser);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using CefSharp;
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+using TweetDick.Forms;
+
+namespace TweetDick{
+    static class Program{
+        private static string HeaderAcceptLanguage{
+            get{
+                string culture = CultureInfo.CurrentCulture.Name;
+
+                if (culture == "en"){
+                    return "en-us,en";
+                }
+                else{
+                    return culture.ToLowerInvariant()+",en;q=0.9";
+                }
+            }
+        }
+
+        [STAThread]
+        private static void Main(){
+            Cef.Initialize(new CefSettings{
+                AcceptLanguageList = HeaderAcceptLanguage,
+                UserAgent = "TweetDick "+Application.ProductVersion,
+                Locale = CultureInfo.CurrentCulture.TwoLetterISOLanguageName
+            });
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormBrowser());
+
+            Application.ApplicationExit += (sender, args) => {
+                Cef.Shutdown();
+            };
+        }
+    }
+}
