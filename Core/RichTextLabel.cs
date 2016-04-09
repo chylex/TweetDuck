5e5343b7d6b8da315849c8871e1a7987bd1254c9
chylex <info@chylex.com>
2016-04-09 17:48:45 +0200

    Add RichTextLabel.Wrap for default RTF text formatting

--- Core/RichTextLabel.cs
+++ Core/RichTextLabel.cs
@@ -3,6 +3,16 @@ using System.Windows.Forms;
 
 namespace TweetDick.Core{
     public partial class RichTextLabel : RichTextBox{
+        /// <summary>
+        /// Wraps the body of a RTF formatted string with default tags and formatting.
+        /// </summary>
+        public static string Wrap(string str){
+            string rtf = @"{\rtf1\ansi\ansicpg1250\deff0\deflang1029{\fonttbl{\f0\fnil\fcharset0 Microsoft Sans Serif;}}";
+            rtf += @"{\*\generator Msftedit 4.20.69.1337;}\viewkind4\uc1\pard\sa200\sl276\slmult1\lang1036\f0\fs20 ";
+            rtf += str;
+            return rtf;
+        }
+
         public RichTextLabel(){
             InitializeComponent();
 
