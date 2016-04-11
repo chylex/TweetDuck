496c1a91a3dd4ef469e215ff2e8df5d22664e64d
chylex <info@chylex.com>
2016-04-11 14:01:53 +0200

    Add a FlatProgressBar control

---
+++ Core/Controls/FlatProgressBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TweetDick.Core.Controls{
+    public partial class FlatProgressBar : ProgressBar{
+        private SolidBrush brush;
+
+        public FlatProgressBar(){
+            SetStyle(ControlStyles.UserPaint,true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer,true);
+        }
+
+        public void SetValueInstant(int value){
+            if (value == Maximum){
+                Value = value;
+                Value = value-1;
+                Value = value;
+            }
+            else{
+                Value = value+1;
+                Value = value;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e){
+            if (brush == null || brush.Color != ForeColor){
+                brush = new SolidBrush(ForeColor);
+            }
+
+            Rectangle rect = e.ClipRectangle;
+            rect.Width = (int)(rect.Width*((double)Value/Maximum));
+            e.Graphics.FillRectangle(brush,rect);
+        }
+    }
+}
