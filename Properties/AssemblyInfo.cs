d3d1fc808e86da2fe712dacb450cdec904634ef6
chylex <info@chylex.com>
2016-04-14 18:09:20 +0200

    Update assembly description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle(Program.BrandName)]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TweetDeck client for Windows")]
+[assembly: AssemblyDescription("TweetDeck client for Windows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct(Program.BrandName)]
