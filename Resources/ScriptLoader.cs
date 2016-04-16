3c43211b25131d8ddd559ef901f9d346126c127a
chylex <info@chylex.com>
2016-04-16 02:02:10 +0200

    Make sure ScriptLoader uses correct root path

--- Resources/ScriptLoader.cs
+++ Resources/ScriptLoader.cs
@@ -7,7 +7,7 @@ namespace TweetDck.Resources{
     static class ScriptLoader{
         public static string LoadResource(string name){
             try{
-                return File.ReadAllText(name,Encoding.UTF8);
+                return File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,name),Encoding.UTF8);
             }catch(Exception ex){
                 MessageBox.Show("Unfortunately, "+Program.BrandName+" could not load the "+name+" file. The program will continue running with limited functionality.\r\n\r\n"+ex.Message,Program.BrandName+" Has Failed :(",MessageBoxButtons.OK,MessageBoxIcon.Error);
                 return null;
