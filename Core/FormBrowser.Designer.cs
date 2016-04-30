d33d65031c79d3100a9a37a3c9cef378e77a4471
chylex <info@chylex.com>
2016-04-30 18:21:28 +0200

    Add a minimum size to the browser Form

--- Core/FormBrowser.Designer.cs
+++ Core/FormBrowser.Designer.cs
@@ -35,9 +35,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(324, 386);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Location = new System.Drawing.Point(-32000, -32000);
+            this.MinimumSize = new System.Drawing.Size(340, 424);
             this.Name = "FormBrowser";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormBrowser_FormClosing);
