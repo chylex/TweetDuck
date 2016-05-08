f5212d88f3038d6ee9a4509bcbe3251efde81abc
chylex <info@chylex.com>
2016-05-08 20:53:08 +0200

    Redesign the About screen

--- Core/Other/FormAbout.Designer.cs
+++ Core/Other/FormAbout.Designer.cs
@@ -25,44 +25,131 @@ namespace TweetDck.Core.Other {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.labelAbout = new TweetDck.Core.Controls.RichTextLabel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelSourceCode = new System.Windows.Forms.LinkLabel();
+            this.labelWebsite = new System.Windows.Forms.LinkLabel();
+            this.tablePanelLinks = new System.Windows.Forms.TableLayoutPanel();
+            this.labelIssues = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            this.tablePanelLinks.SuspendLayout();
             this.SuspendLayout();
             // 
-            // labelAbout
+            // pictureLogo
             // 
-            this.labelAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pictureLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureLogo.ErrorImage = null;
+            this.pictureLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureLogo.Image")));
+            this.pictureLogo.InitialImage = null;
+            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(96, 96);
+            this.pictureLogo.TabIndex = 0;
+            this.pictureLogo.TabStop = false;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelAbout.BackColor = System.Drawing.Color.White;
-            this.labelAbout.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.labelAbout.Location = new System.Drawing.Point(12, 12);
-            this.labelAbout.Name = "labelAbout";
-            this.labelAbout.ReadOnly = true;
-            this.labelAbout.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.labelAbout.Size = new System.Drawing.Size(360, 126);
-            this.labelAbout.TabIndex = 0;
-            this.labelAbout.TabStop = false;
-            this.labelAbout.Text = "";
-            this.labelAbout.Click += new System.EventHandler(this.labelAbout_Click);
+            this.labelDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelDescription.Location = new System.Drawing.Point(114, 12);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(232, 109);
+            this.labelDescription.TabIndex = 1;
+            // 
+            // labelSourceCode
+            // 
+            this.labelSourceCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelSourceCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelSourceCode.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
+            this.labelSourceCode.Location = new System.Drawing.Point(117, 0);
+            this.labelSourceCode.Margin = new System.Windows.Forms.Padding(0);
+            this.labelSourceCode.Name = "labelSourceCode";
+            this.labelSourceCode.Size = new System.Drawing.Size(99, 16);
+            this.labelSourceCode.TabIndex = 3;
+            this.labelSourceCode.Text = "Source Code";
+            this.labelSourceCode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelSourceCode.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnLinkClicked);
+            // 
+            // labelWebsite
+            // 
+            this.labelWebsite.AutoSize = true;
+            this.labelWebsite.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelWebsite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelWebsite.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
+            this.labelWebsite.Location = new System.Drawing.Point(0, 0);
+            this.labelWebsite.Margin = new System.Windows.Forms.Padding(0);
+            this.labelWebsite.Name = "labelWebsite";
+            this.labelWebsite.Size = new System.Drawing.Size(117, 16);
+            this.labelWebsite.TabIndex = 2;
+            this.labelWebsite.Text = "Official Website";
+            this.labelWebsite.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelWebsite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnLinkClicked);
+            // 
+            // tablePanelLinks
+            // 
+            this.tablePanelLinks.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tablePanelLinks.ColumnCount = 3;
+            this.tablePanelLinks.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.16F));
+            this.tablePanelLinks.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.7F));
+            this.tablePanelLinks.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.14F));
+            this.tablePanelLinks.Controls.Add(this.labelIssues, 2, 0);
+            this.tablePanelLinks.Controls.Add(this.labelWebsite, 0, 0);
+            this.tablePanelLinks.Controls.Add(this.labelSourceCode, 1, 0);
+            this.tablePanelLinks.Location = new System.Drawing.Point(12, 124);
+            this.tablePanelLinks.Name = "tablePanelLinks";
+            this.tablePanelLinks.RowCount = 1;
+            this.tablePanelLinks.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tablePanelLinks.Size = new System.Drawing.Size(334, 16);
+            this.tablePanelLinks.TabIndex = 4;
+            // 
+            // labelIssues
+            // 
+            this.labelIssues.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelIssues.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelIssues.LinkArea = new System.Windows.Forms.LinkArea(0, 0);
+            this.labelIssues.Location = new System.Drawing.Point(216, 0);
+            this.labelIssues.Margin = new System.Windows.Forms.Padding(0);
+            this.labelIssues.Name = "labelIssues";
+            this.labelIssues.Size = new System.Drawing.Size(118, 16);
+            this.labelIssues.TabIndex = 4;
+            this.labelIssues.Text = "Report an Issue";
+            this.labelIssues.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labelIssues.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.OnLinkClicked);
             // 
             // FormAbout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(384, 150);
-            this.Controls.Add(this.labelAbout);
+            this.ClientSize = new System.Drawing.Size(358, 152);
+            this.Controls.Add(this.tablePanelLinks);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.pictureLogo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormAbout";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            this.tablePanelLinks.ResumeLayout(false);
+            this.tablePanelLinks.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Controls.RichTextLabel labelAbout;
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.LinkLabel labelSourceCode;
+        private System.Windows.Forms.LinkLabel labelWebsite;
+        private System.Windows.Forms.TableLayoutPanel tablePanelLinks;
+        private System.Windows.Forms.LinkLabel labelIssues;
+
     }
 }
--- Core/Other/FormAbout.cs
+++ Core/Other/FormAbout.cs
@@ -1,43 +1,25 @@
-﻿using System;
-using System.Text;
-using System.Windows.Forms;
-using TweetDck.Core.Controls;
+﻿using System.Windows.Forms;
 using TweetDck.Core.Utils;
 
 namespace TweetDck.Core.Other{
     sealed partial class FormAbout : Form{
         private const string GitHubLink = "https://github.com/chylex/TweetDuck";
+        private const string IssuesLink = "https://github.com/chylex/TweetDuck/issues";
 
         public FormAbout(){
             InitializeComponent();
 
             Text = "About "+Program.BrandName+" "+Program.VersionTag;
 
-            StringBuilder build = new StringBuilder();
-            build.Append(@"\fs22").Append(Program.BrandName).Append(@" was created by chylex as a replacement to the discontinued TweetDeck client for Windows, and is released under the MIT license.\par ");
-            build.Append(@"Official Website: ").Append(RichTextLabel.AddLink(Program.Website)).Append(@"\line ");
-            build.Append(@"Source Code: ").Append(RichTextLabel.AddLink(GitHubLink));
-
-            labelAbout.Rtf = RichTextLabel.Wrap(build.ToString());
+            labelDescription.Text = Program.BrandName+" was created by chylex as a replacement to the discontinued official TweetDeck client for Windows.\n\nThe program is available for free under the open source MIT license.";
+            
+            labelWebsite.Links.Add(new LinkLabel.Link(0,labelWebsite.Text.Length,Program.Website));
+            labelSourceCode.Links.Add(new LinkLabel.Link(0,labelSourceCode.Text.Length,GitHubLink));
+            labelIssues.Links.Add(new LinkLabel.Link(0,labelIssues.Text.Length,IssuesLink));
         }
 
-        private void labelAbout_Click(object sender, EventArgs e){ // LinkClicked isn't working so fuck that
-            if (Cursor.Current != Cursors.Hand)return;
-
-            // I don't even give a fuck, someone else PR a proper fix please
-            int index = labelAbout.GetCharIndexFromPosition(((MouseEventArgs)e).Location);
-
-            if (IsClickingOn(index,Program.Website)){
-                BrowserUtils.OpenExternalBrowser(Program.Website);
-            }
-            else if (IsClickingOn(index,GitHubLink)){
-                BrowserUtils.OpenExternalBrowser(GitHubLink);
-            }
-        }
-
-        private bool IsClickingOn(int index, string substringSearch){
-            int substringIndex = labelAbout.Text.IndexOf(substringSearch,StringComparison.Ordinal);
-            return index >= substringIndex && index <= substringIndex+substringSearch.Length;
+        private void OnLinkClicked(object sender, LinkLabelLinkClickedEventArgs e){
+            BrowserUtils.OpenExternalBrowser(e.Link.LinkData as string);
         }
     }
 }
