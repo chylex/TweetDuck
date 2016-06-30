6e0717bf227ce19ed3d3fc786bddffaf3b174113
chylex <info@chylex.com>
2016-06-30 19:39:49 +0200

    Add a WindowState class for easier Form saving and restoring

---
+++ Core/Utils/WindowState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TweetDck.Core.Utils{
+    [Serializable]
+    class WindowState{
+        private Rectangle rect;
+        private bool isMaximized;
+
+        public void Save(Form form){
+            rect = form.WindowState == FormWindowState.Normal ? form.DesktopBounds : form.RestoreBounds;
+            isMaximized = form.WindowState == FormWindowState.Maximized;
+        }
+
+        public void Restore(Form form){
+            if (rect != Rectangle.Empty){
+                form.DesktopBounds = rect;
+                form.WindowState = isMaximized ? FormWindowState.Maximized : FormWindowState.Normal;
+            }
+
+            if (rect == Rectangle.Empty || !Screen.AllScreens.Any(screen => screen.WorkingArea.IntersectsWith(form.Bounds))){
+                form.DesktopBounds = Screen.PrimaryScreen.WorkingArea;
+                form.WindowState = FormWindowState.Maximized;
+                Save(form);
+            }
+        }
+    }
+}
