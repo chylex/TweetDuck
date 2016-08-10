300c0c619563505efafebd0cf4a427147350d253
chylex <info@chylex.com>
2016-08-10 17:16:39 +0200

    Minor TweetDeck UI fixes (cut off account names and verified badge icons)

--- Resources/Scripts/code.js
+++ Resources/Scripts/code.js
@@ -401,7 +401,10 @@
   (function(){
     var styleOfficial = document.createElement("style");
     document.head.appendChild(styleOfficial);
+    
     styleOfficial.sheet.insertRule("a[data-full-url] { word-break: break-all; }",0); // break long urls
+    styleOfficial.sheet.insertRule(".column-nav-link .attribution { position: absolute; }",0); // fix cut off account names
+    styleOfficial.sheet.insertRule(".txt-base-smallest .badge-verified:before { height: 13px !important; }",0); // fix cut off badge icon
     
     if ($TD.hasCustomBrowserCSS){
       var styleCustom = document.createElement("style");
