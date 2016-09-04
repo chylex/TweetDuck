85d5160782acd1ff6683b744c4b96f0ef7f50598
chylex <info@chylex.com>
2016-09-04 03:38:42 +0200

    Add plugin identifier to the object itself

--- Plugins/PluginScriptGenerator.cs
+++ Plugins/PluginScriptGenerator.cs
@@ -7,7 +7,7 @@ namespace TweetDck.Plugins{
         }
 
         public static string GeneratePlugin(string pluginIdentifier, string pluginContents, int pluginToken, PluginEnvironment environment){
-            StringBuilder build = new StringBuilder(pluginIdentifier.Length+pluginContents.Length+150);
+            StringBuilder build = new StringBuilder(2*pluginIdentifier.Length+pluginContents.Length+165);
 
             build.Append("(function(").Append(environment.GetScriptVariables()).Append("){");
             
@@ -16,6 +16,7 @@ namespace TweetDck.Plugins{
             build.Append("obj:new class extends PluginBase{").Append(pluginContents).Append("}");
             build.Append("};");
             
+            build.Append("tmp.obj.$id=\"").Append(pluginIdentifier).Append("\";");
             build.Append("tmp.obj.$token=").Append(pluginToken).Append(";");
             build.Append("window.TD_PLUGINS.install(tmp);");
 
