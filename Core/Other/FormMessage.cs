85d09c4b5ef0729d74d1a163b068336d7d432328
chylex <info@chylex.com>
2016-09-20 07:47:16 +0200

    Add a FormMessage class for custom message boxes

---
+++ Core/Other/FormMessage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TweetDck.Core.Other{
+    sealed partial class FormMessage : Form{
+        private readonly Icon icon;
+        private readonly bool isReady;
+
+        private int realFormWidth, minFormWidth;
+        private int buttonCount;
+        private int prevLabelWidth, prevLabelHeight;
+        private bool wasLabelMultiline;
+
+        public FormMessage(string caption, string text, MessageBoxIcon messageIcon){
+            InitializeComponent();
+
+            this.prevLabelWidth = labelMessage.Width;
+            this.prevLabelHeight = labelMessage.Height;
+            this.minFormWidth = 18;
+
+            switch(messageIcon){
+                case MessageBoxIcon.Information:
+                    icon = SystemIcons.Information;
+                    break;
+
+                case MessageBoxIcon.Warning:
+                    icon = SystemIcons.Warning;
+                    break;
+                    
+                case MessageBoxIcon.Error:
+                    icon = SystemIcons.Error;
+                    break;
+
+                default:
+                    icon = null;
+                    labelMessage.Location = new Point(labelMessage.Location.X-32, labelMessage.Location.Y);
+                    break;
+            }
+
+            this.isReady = true;
+
+            this.Text = caption;
+            this.labelMessage.Text = text;
+        }
+
+        public int AddButton(string title){
+            Button button = new Button{
+                Anchor = AnchorStyles.Bottom | AnchorStyles.Right,
+                Location = new Point(Width-112-buttonCount*96, 12),
+                Size = new Size(88, 26),
+                TabIndex = buttonCount,
+                Text = title,
+                UseVisualStyleBackColor = true
+            };
+
+            panelActions.Controls.Add(button);
+
+            minFormWidth += 96;
+            Width = Math.Max(realFormWidth, minFormWidth);
+
+            return buttonCount++;
+        }
+
+        private void labelMessage_SizeChanged(object sender, EventArgs e){
+            if (!isReady){
+                return;
+            }
+
+            bool isMultiline = labelMessage.Height > labelMessage.MinimumSize.Height;
+
+            if (isMultiline && !wasLabelMultiline){
+                labelMessage.Location = new Point(labelMessage.Location.X, labelMessage.Location.Y-8);
+                prevLabelHeight += 8;
+            }
+            else if (!isMultiline && wasLabelMultiline){
+                labelMessage.Location = new Point(labelMessage.Location.X, labelMessage.Location.Y+8);
+                prevLabelHeight -= 8;
+            }
+
+            realFormWidth = Width-(icon == null ? 32+35+(labelMessage.Margin.Left-labelMessage.Margin.Right) : 0)+labelMessage.Margin.Right+labelMessage.Width-prevLabelWidth;
+            Width = Math.Max(realFormWidth, minFormWidth);
+            Height += labelMessage.Height-prevLabelHeight;
+
+            prevLabelWidth = labelMessage.Width;
+            prevLabelHeight = labelMessage.Height;
+            wasLabelMultiline = isMultiline;
+        }
+
+        protected override void OnPaint(PaintEventArgs e){
+            if (icon != null){
+                e.Graphics.DrawIcon(icon, 25, 26);
+            }
+
+            base.OnPaint(e);
+        }
+    }
+}
