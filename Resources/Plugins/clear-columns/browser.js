c46aafdab6d3f17dc96accf0758afd6bb0f77d7b
chylex <info@chylex.com>
2016-09-25 15:31:18 +0200

    Add column resetting to clear-columns plugin

--- Resources/Plugins/clear-columns/browser.js
+++ Resources/Plugins/clear-columns/browser.js
@@ -11,35 +11,72 @@ enabled(){
     TD.controller.stats.columnActionClick("clear");
   };
   
-  var clearAllColumns = () => {
-    Object.keys(TD.controller.columnManager.getAll()).forEach(key => clearColumn(key));
+  var resetColumn = (columnName) => {
+    var col = TD.controller.columnManager.get(columnName);
+    col.model.setClearedTimestamp(0);
+    col.reloadTweets();
+  };
+  
+  var forEachColumn = (func) => {
+    Object.keys(TD.controller.columnManager.getAll()).forEach(func);
   };
   
   var replaceMustache = (key, search, replace) => {
     TD.mustaches[key] = TD.mustaches[key].replace(search, replace);
   };
   
+  var wasShiftPressed = false;
+  
+  var updateShiftState = (pressed) => {
+    if (pressed != wasShiftPressed){
+      wasShiftPressed = pressed;
+      
+      if (pressed){
+        $(document).on("mousemove", this.eventKeyUp);
+      }
+      else{
+        $(document).off("mousemove", this.eventKeyUp);
+      }
+      
+      $("#clear-columns-btn-all").text(pressed ? "Reset all" : "Clear all");
+    }
+  };
+  
   // prepare event handlers
-  this.eventClearSingle = function(){
-    clearColumn($(this).closest(".js-column").attr("data-column"));
+  this.eventClickSingle = function(e){
+    var name = $(this).closest(".js-column").attr("data-column");
+    e.shiftKey ? resetColumn(name) : clearColumn(name);
   };
   
-  this.eventClearAll = function(){
-    clearAllColumns();
+  this.eventClickAll = function(e){
+    forEachColumn(e.shiftKey ? resetColumn : clearColumn);
   };
   
-  this.eventKeys = function(e){
-    if (e.keyCode === 46 && (document.activeElement === null || document.activeElement === document.body)){ // 46 = delete
+  this.eventKeyDown = function(e){
+    if (!(document.activeElement === null || document.activeElement === document.body)){
+      return;
+    }
+    
+    updateShiftState(e.shiftKey);
+    
+    if (e.keyCode === 46){ // 46 = delete
       if (e.altKey){
-        clearAllColumns();
+        forEachColumn(e.shiftKey ? resetColumn : clearColumn);
       }
       else{
         var focusedColumn = $(".js-column.is-focused");
         
         if (focusedColumn.length){
-          clearColumn(focusedColumn.attr("data-column"));
+          var name = focusedColumn.attr("data-column");
+          e.shiftKey ? resetColumn(name) : clearColumn(name);
         }
       }
+    }
+  };
+  
+  this.eventKeyUp = function(e){
+    if (!e.shiftKey){
+      updateShiftState(false);
     }
   };
   
@@ -67,19 +104,21 @@ enabled(){
   css.insert(".column-type-message .column-title { margin-right: 115px !important; }");
   css.insert(".mark-all-read-link { right: 59px !important; }");
   css.insert(".open-compose-dm-link { right: 90px !important; }");
+  css.insert(".btn-options-tray button[data-action='clear'] { display: none !important; }");
 }
 
 ready(){
   // setup events
-  $(document).on("click", "[data-action='td-clearcolumns-dosingle']", this.eventClearSingle);
-  $(document).on("click", "[data-action='td-clearcolumns-doall']", this.eventClearAll);
-  $(document).on("keydown", this.eventKeys);
+  $(document).on("click", "[data-action='td-clearcolumns-dosingle']", this.eventClickSingle);
+  $(document).on("click", "[data-action='td-clearcolumns-doall']", this.eventClickAll);
+  $(document).on("keydown", this.eventKeyDown);
+  $(document).on("keyup", this.eventKeyUp);
   
   // add clear all button
   $("nav.app-navigator").first().append([
     '<a class="link-clean cf app-nav-link padding-hl" data-title="Clear all" data-action="td-clearcolumns-doall">',
     '<div class="obj-left"><i class="icon icon-large icon-clear-timeline"></i></div>',
-    '<div class="nbfc padding-ts hide-condensed">Clear all</div>',
+    '<div id="clear-columns-btn-all" class="nbfc padding-ts hide-condensed">Clear all</div>',
     '</a></nav>'
   ].join(""));
 }
