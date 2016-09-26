8d0fa030f8185b93f3ccecd94d0674a55f1a8862
chylex <info@chylex.com>
2016-09-26 16:16:20 +0200

    Remove d3dcompiler_43.dll that used to be required for Win XP

--- _postbuild.bat
+++ _postbuild.bat
@@ -1,5 +1,6 @@
 del "bin\x86\Release\*.xml"
 del "bin\x86\Release\devtools_resources.pak"
+del "bin\x86\Release\d3dcompiler_43.dll"
 
 del "bin\x86\Release\TweetDuck.Browser.exe"
 ren "bin\x86\Release\CefSharp.BrowserSubprocess.exe" "TweetDuck.Browser.exe"
