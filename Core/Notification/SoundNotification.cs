b7c02d1cf8ff4eee4167d61035cd774f3fb60985
chylex <contact@chylex.com>
2017-03-26 15:56:49 +0200

    Rewrite SoundNotification to use WMPLib

--- Core/Notification/SoundNotification.cs
+++ Core/Notification/SoundNotification.cs
@@ -1,52 +1,100 @@
 ﻿using System;
-using System.IO;
-using System.Media;
+using System.Runtime.InteropServices;
+using WMPLib;
 
 namespace TweetDck.Core.Notification{
-    sealed class SoundNotification : IDisposable{
+    sealed class SoundNotification : IDisposable{ // TODO test on windows server
+        public const string SupportedFormats = "*.wav;*.mp3;*.mp2;*.m4a;*.mid;*.midi;*.rmi;*.wma;*.aif;*.aifc;*.aiff;*.snd;*.au";
 
-        private SoundPlayer notificationSound;
-        private bool ignoreNotificationSoundError;
-
-        public event EventHandler<PlaybackErrorEventArgs> PlaybackError;
-
-        public void Play(string file){
-            if (notificationSound == null){
-                notificationSound = new SoundPlayer{
-                    LoadTimeout = 5000
-                };
+        public int Volume{
+            get{
+                return player.settings.volume;
             }
 
-            if (notificationSound.SoundLocation != file){
-                notificationSound.SoundLocation = file;
-                ignoreNotificationSoundError = false;
-            }
-
-            try{
-                notificationSound.Play();
-            }catch(FileNotFoundException e){
-                OnNotificationSoundError("File not found: "+e.FileName);
-            }catch(InvalidOperationException){
-                OnNotificationSoundError("File is not a valid sound file.");
-            }catch(TimeoutException){
-                OnNotificationSoundError("File took too long to load.");
+            set{
+                player.settings.volume = value;
             }
         }
 
+        public event EventHandler<PlaybackErrorEventArgs> PlaybackError;
 
+        private readonly WindowsMediaPlayer player;
+        private bool wasTryingToPlay;
+        private bool ignorePlaybackError;
+
+        public SoundNotification(){
+            player = new WindowsMediaPlayer();
+            player.settings.autoStart = false;
+            player.settings.enableErrorDialogs = false;
+            player.settings.invokeURLs = false;
+            player.MediaChange += player_MediaChange;
+            player.MediaError += player_MediaError;
+        }
+
+        public void Play(string file){
+            wasTryingToPlay = true;
+
+            if (player.URL != file){
+                player.close();
+                player.URL = file;
+                ignorePlaybackError = false;
+            }
+            else{
+                player.controls.stop();
+            }
+            
+            player.controls.play();
+        }
+
+        public void Stop(){
+            player.controls.stop();
+        }
+
+        private void player_MediaChange(object item){
+            IWMPMedia2 media = item as IWMPMedia2;
+
+            if (media == null){
+                OnNotificationSoundError("Unknown error.");
+            }
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            else if (media.Error == null && media.duration == 0.0){
+                OnNotificationSoundError("File does not contain an audio track.");
+            }
+            else if (media.Error != null){
+                OnNotificationSoundError(media.Error);
+            }
+        }
+
+        private void player_MediaError(object pMediaObject){
+            IWMPMedia2 media = pMediaObject as IWMPMedia2;
+
+            if (media == null){
+                OnNotificationSoundError("Unknown error.");
+            }
+            else if (media.Error != null){
+                OnNotificationSoundError(media.Error);
+            }
+        }
+
+        private void OnNotificationSoundError(IWMPErrorItem error){
+            OnNotificationSoundError(error.errorCode == -1072885353 ? "Invalid media file." : error.errorDescription);
+        }
 
         private void OnNotificationSoundError(string message){
-            if (!ignoreNotificationSoundError && PlaybackError != null){
-                PlaybackErrorEventArgs args = new PlaybackErrorEventArgs(message);
-                PlaybackError(this, args);
-                ignoreNotificationSoundError = args.Ignore;
+            if (wasTryingToPlay){
+                wasTryingToPlay = false;
+
+                if (!ignorePlaybackError && PlaybackError != null){
+                    PlaybackErrorEventArgs args = new PlaybackErrorEventArgs(message);
+                    PlaybackError(this, args);
+                    ignorePlaybackError = args.Ignore;
+                }
             }
         }
 
         public void Dispose(){
-            if (notificationSound != null){
-                notificationSound.Dispose();
-            }
+            player.close();
+            Marshal.ReleaseComObject(player);
         }
 
         public class PlaybackErrorEventArgs : EventArgs{
