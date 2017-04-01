130159f06cd8713e7f7d660fb5c9a2792d10cd89
chylex <info@chylex.com>
2017-04-01 18:43:23 +0200

    Add InjectedHTML utility class with unit tests

---
+++ Core/Utils/InjectedHTML.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TweetDck.Core.Utils{
+    class InjectedHTML{
+        public enum Position{
+            Before, After
+        }
+
+        private readonly Position position;
+        private readonly string search;
+        private readonly string html;
+
+        public InjectedHTML(Position position, string search, string html){
+            this.position = position;
+            this.search = search;
+            this.html = html;
+        }
+
+        public string Inject(string targetHTML){
+            int index = targetHTML.IndexOf(search, StringComparison.Ordinal);
+
+            if (index == -1){
+                return targetHTML;
+            }
+
+            int cutIndex;
+
+            switch(position){
+                case Position.Before: cutIndex = index; break;
+                case Position.After: cutIndex = index+search.Length; break;
+                default: return targetHTML;
+            }
+
+            return targetHTML.Insert(cutIndex, html);
+        }
+    }
+}
