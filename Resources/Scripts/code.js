b5e58db242505bb532a96cc70e0f0d875aed5629
chylex <contact@chylex.com>
2017-05-19 14:24:51 +0200

    Rewrite pre-login page skip to be instant

--- Resources/Scripts/code.js
+++ Resources/Scripts/code.js
@@ -704,7 +704,7 @@
   //
   // Block: Skip the initial pre-login page.
   //
-  $(document).on("uiLoginFormImpression", function(){
-    location.href = $("a.btn", ".js-login-form").first().attr("href");
-  });
+  TD.controller.init.showLogin = function(){
+    location.href = "https://twitter.com/login?hide_message=true&redirect_after_login=https%3A%2F%2Ftweetdeck.twitter.com%2F%3Fvia_twitter_login%3Dtrue";
+  };
 })($, $TD, $TDX, TD);
