63107111367f04422dfa6b58f6c6acb627f2227d
chylex <contact@chylex.com>
2017-06-20 11:48:44 +0200

    Add a vertical label control

---
+++ Core/Controls/LabelVertical.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TweetDuck.Core.Controls{
+    sealed class LabelVertical : Label{
+        public int LineHeight { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e){
+            int y = (int)Math.Floor((ClientRectangle.Height-Text.Length*LineHeight)/2F)-2; // 2 = random
+
+            using(Brush brush = new SolidBrush(ForeColor)){
+                foreach(char chr in Text){
+                    string str = chr.ToString();
+                    float x = (ClientRectangle.Width-e.Graphics.MeasureString(str, Font).Width)/2F;
+
+                    e.Graphics.DrawString(str, Font, brush, x, y);
+                    y += LineHeight;
+                }
+            }
+        }
+    }
+}
