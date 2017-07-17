55eea88ace6e844dd61545e51a632c91b85250de
chylex <contact@chylex.com>
2017-07-17 05:10:06 +0200

    Add twitter image link & download methods to TwitterUtils

--- Core/Utils/TwitterUtils.cs
+++ Core/Utils/TwitterUtils.cs
@@ -1,5 +1,8 @@
-﻿using System.Drawing;
-using CefSharp;
+﻿using CefSharp;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using TweetDuck.Core.Other;
 
 namespace TweetDuck.Core.Utils{
     static class TwitterUtils{
@@ -12,12 +15,53 @@ namespace TweetDuck.Core.Utils{
             "tweetdeck", "TweetDeck", "tweetduck", "TweetDuck", "TD"
         };
 
+        public enum ImageQuality{
+            Large, Orig
+        }
+
         public static bool IsTweetDeckWebsite(IFrame frame){
             return frame.Url.Contains("//tweetdeck.twitter.com/");
         }
 
         public static bool IsTwitterWebsite(IFrame frame){
             return frame.Url.Contains("//twitter.com/");
+        }
+
+        private static string ExtractImageBaseLink(string url){
+            int dot = url.LastIndexOf('.');
+            return dot == -1 ? url : StringUtils.ExtractBefore(url, ':', dot);
+        }
+
+        public static string GetImageLink(string url, ImageQuality quality){
+            string result = ExtractImageBaseLink(url);
+
+            if (result != url){
+                switch(quality){
+                    case ImageQuality.Large: result += ":large"; break;
+                    case ImageQuality.Orig: result += ":orig"; break;
+                }
+            }
+
+            return result;
+        }
+
+        public static void DownloadImage(string url, ImageQuality quality){
+            string file = BrowserUtils.GetFileNameFromUrl(ExtractImageBaseLink(url));
+            string ext = Path.GetExtension(file);
+            
+            using(SaveFileDialog dialog = new SaveFileDialog{
+                AutoUpgradeEnabled = true,
+                OverwritePrompt = true,
+                Title = "Save image",
+                FileName = file,
+                Filter = "Image ("+(string.IsNullOrEmpty(ext) ? "unknown" : ext)+")|*.*"
+            }){
+                if (dialog.ShowDialog() == DialogResult.OK){
+                    BrowserUtils.DownloadFileAsync(GetImageLink(url, quality), dialog.FileName, null, ex => {
+                        FormMessage.Error("Image Download", "An error occurred while downloading the image: "+ex.Message, FormMessage.OK);
+                    });
+                }
+            }
         }
     }
 }
