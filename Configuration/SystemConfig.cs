93c1cbd2314cc71f08b1447ab28bf2769be5f687
chylex <contact@chylex.com>
2017-07-30 19:54:28 +0200

    Update SystemConfig to use FileSerializer and migrate old files

--- Configuration/SystemConfig.cs
+++ Configuration/SystemConfig.cs
@@ -1,43 +1,40 @@
 ﻿using System;
 using System.IO;
 using TweetDuck.Core.Utils;
+using TweetDuck.Data.Serialization;
 
 namespace TweetDuck.Configuration{
     sealed class SystemConfig{
+        private static readonly FileSerializer<SystemConfig> Serializer = new FileSerializer<SystemConfig>{
+            OnReadUnknownProperty = (obj, property, value) => false
+        };
+
         public static readonly bool IsHardwareAccelerationSupported = File.Exists(Path.Combine(Program.ProgramPath, "libEGL.dll")) &&
                                                                       File.Exists(Path.Combine(Program.ProgramPath, "libGLESv2.dll"));
 
+        // CONFIGURATION DATA
+
+        private bool _hardwareAcceleration = true;
+
+        // SPECIAL PROPERTIES
+        
         public bool HardwareAcceleration{
-            get => hardwareAcceleration && IsHardwareAccelerationSupported;
-            set => hardwareAcceleration = value;
+            get => _hardwareAcceleration && IsHardwareAccelerationSupported;
+            set => _hardwareAcceleration = value;
         }
 
-        private readonly string file;
+        // END OF CONFIG
 
-        private bool hardwareAcceleration;
+        private readonly string file;
         
         private SystemConfig(string file){
             this.file = file;
-
-            HardwareAcceleration = true;
-        }
-
-        private void WriteToStream(Stream stream){
-            stream.WriteByte((byte)(HardwareAcceleration ? 1 : 0));
-        }
-
-        private void ReadFromStream(Stream stream){
-            HardwareAcceleration = stream.ReadByte() > 0;
         }
 
         public bool Save(){
             try{
                 WindowsUtils.CreateDirectoryForFile(file);
-
-                using(Stream stream = new FileStream(file, FileMode.Create, FileAccess.Write, FileShare.None)){
-                    WriteToStream(stream);
-                }
-
+                Serializer.Write(file, this);
                 return true;
             }catch(Exception e){
                 Program.Reporter.HandleException("Configuration Error", "Could not save the system configuration file.", true, e);
@@ -49,11 +46,20 @@ namespace TweetDuck.Configuration{
             SystemConfig config = new SystemConfig(file);
             
             try{
-                using(Stream stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read)){
-                    config.ReadFromStream(stream);
-                }
+                Serializer.Read(file, config);
+                return config;
             }catch(FileNotFoundException){
             }catch(DirectoryNotFoundException){
+            }catch(FormatException){
+                try{
+                    using(Stream stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read)){
+                        config.HardwareAcceleration = stream.ReadByte() > 0;
+                    }
+
+                    config.Save();
+                }catch(Exception e){
+                    Program.Reporter.HandleException("Configuration Error", "Could not update the system configuration file.", true, e);
+                }
             }catch(Exception e){
                 Program.Reporter.HandleException("Configuration Error", "Could not open the system configuration file. If you continue, you will lose system specific configuration such as Hardware Acceleration.", true, e);
             }
