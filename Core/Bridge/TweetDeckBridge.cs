44397b2d45f0aa7ee0285454c61eab7410a3d943
chylex <contact@chylex.com>
2017-08-31 14:07:46 +0200

    Fix parameter name in TweetDeckBridge

--- Core/Bridge/TweetDeckBridge.cs
+++ Core/Bridge/TweetDeckBridge.cs
@@ -72,10 +72,10 @@ namespace TweetDuck.Core.Bridge{
             form.InvokeAsyncSafe(form.OpenContextMenu);
         }
 
-        public void OnTweetPopup(string columnKey, string chirpId, string columnName, string tweetHtml, int tweetCharacters, string tweetUrl, string quoteUrl){
+        public void OnTweetPopup(string columnId, string chirpId, string columnName, string tweetHtml, int tweetCharacters, string tweetUrl, string quoteUrl){
             notification.InvokeAsyncSafe(() => {
                 form.OnTweetNotification();
-                notification.ShowNotification(new TweetNotification(columnKey, chirpId, columnName, tweetHtml, tweetCharacters, tweetUrl, quoteUrl));
+                notification.ShowNotification(new TweetNotification(columnId, chirpId, columnName, tweetHtml, tweetCharacters, tweetUrl, quoteUrl));
             });
         }
 
