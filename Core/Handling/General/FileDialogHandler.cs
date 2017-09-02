13ea388f5e729640a6b99f25d255e59c0d9a7ff7
chylex <contact@chylex.com>
2017-09-02 20:51:58 +0200

    Fix upload dialog to include 'All Supported Formats' instead of separating them

---
+++ Core/Handling/General/FileDialogHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using CefSharp;
+
+namespace TweetDuck.Core.Handling.General{
+    sealed class FileDialogHandler : IDialogHandler{
+        public bool OnFileDialog(IWebBrowser browserControl, IBrowser browser, CefFileDialogMode mode, string title, string defaultFilePath, List<string> acceptFilters, int selectedAcceptFilter, IFileDialogCallback callback){
+            CefFileDialogMode dialogType = mode & CefFileDialogMode.TypeMask;
+
+            if (dialogType == CefFileDialogMode.Open || dialogType == CefFileDialogMode.OpenMultiple){
+                string allFilters = string.Join(";", acceptFilters.Select(filter => "*"+filter));
+
+                using(OpenFileDialog dialog = new OpenFileDialog{
+                    AutoUpgradeEnabled = true,
+                    DereferenceLinks = true,
+                    Multiselect = dialogType == CefFileDialogMode.OpenMultiple,
+                    Title = "Open Files",
+                    Filter = $"All Supported Formats ({allFilters})|{allFilters}|All Files (*.*)|*.*"
+                }){
+                    if (dialog.ShowDialog() == DialogResult.OK){
+                        callback.Continue(acceptFilters.FindIndex(filter => filter == Path.GetExtension(dialog.FileName)), dialog.FileNames.ToList());
+                    }
+                    else{
+                        callback.Cancel();
+                    }
+
+                    callback.Dispose();
+                }
+
+                return true;
+            }
+            else{
+                callback.Dispose();
+                return false;
+            }
+        }
+    }
+}
