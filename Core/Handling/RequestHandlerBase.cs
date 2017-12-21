f66ff1000a7b9f76d0dfcbfd5f84a2cf06478d10
chylex <contact@chylex.com>
2017-12-21 22:45:29 +0100

    Replace AbstractRequestHandler with CefSharp's implementation

--- Core/Handling/RequestHandlerBase.cs
+++ Core/Handling/RequestHandlerBase.cs
@@ -1,8 +1,9 @@
 ﻿using CefSharp;
+using CefSharp.Handler;
 using TweetDuck.Core.Handling.General;
 
 namespace TweetDuck.Core.Handling{
-    class RequestHandlerBase : AbstractRequestHandler{
+    class RequestHandlerBase : DefaultRequestHandler{
         public override bool OnOpenUrlFromTab(IWebBrowser browserControl, IBrowser browser, IFrame frame, string targetUrl, WindowOpenDisposition targetDisposition, bool userGesture){
             return LifeSpanHandler.HandleLinkClick(browserControl, targetDisposition, targetUrl);
         }
