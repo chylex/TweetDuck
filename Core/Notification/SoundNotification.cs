2d4bbf2a6fe9fc44cdb08ceaf9cdeedfc64826c2
chylex <contact@chylex.com>
2018-01-14 00:08:43 +0100

    Fix sound notification extension detection and add warning to mp3 files

--- Core/Notification/SoundNotification.cs
+++ Core/Notification/SoundNotification.cs
@@ -14,36 +14,40 @@ namespace TweetDuck.Core.Notification{
             string mimeType;
 
             switch(Path.GetExtension(path)){
-                case "weba":
-                case "webm": mimeType = "audio/webm"; break;
-                case "wav": mimeType = "audio/wav"; break;
-                case "ogg": mimeType = "audio/ogg"; break;
-                case "flac": mimeType = "audio/flac"; break;
-                case "opus": mimeType = "audio/ogg; codecs=opus"; break;
+                case ".weba":
+                case ".webm": mimeType = "audio/webm"; break;
+                case ".wav": mimeType = "audio/wav"; break;
+                case ".ogg": mimeType = "audio/ogg"; break;
+                case ".flac": mimeType = "audio/flac"; break;
+                case ".opus": mimeType = "audio/ogg; codecs=opus"; break;
+                case ".mp3": TryShowError("MP3 sound notifications are temporarily unsupported, please convert the file to another format, such as .ogg, .wav, or .flac."); return null;
                 default: mimeType = null; break;
             }
 
             try{
                 return ResourceHandler.FromFilePath(path, mimeType);
             }catch{
-                FormBrowser browser = FormManager.TryFind<FormBrowser>();
-
-                browser?.InvokeAsyncSafe(() => {
-                    using(FormMessage form = new FormMessage("Sound Notification Error", "Could not find custom notification sound file:\n"+path, MessageBoxIcon.Error)){
-                        form.AddButton(FormMessage.Ignore, ControlType.Cancel | ControlType.Focused);
-                        
-                        Button btnViewOptions = form.AddButton("View Options");
-                        btnViewOptions.Width += 16;
-                        btnViewOptions.Location = new Point(btnViewOptions.Location.X-16, btnViewOptions.Location.Y);
-
-                        if (form.ShowDialog() == DialogResult.OK && form.ClickedButton == btnViewOptions){
-                            browser.OpenSettings(typeof(TabSettingsSounds));
-                        }
-                    }
-                });
-
+                TryShowError("Could not find custom notification sound file:\n"+path);
                 return null;
             }
+        }
+
+        private static void TryShowError(string message){
+            FormBrowser browser = FormManager.TryFind<FormBrowser>();
+
+            browser?.InvokeAsyncSafe(() => {
+                using(FormMessage form = new FormMessage("Sound Notification Error", message, MessageBoxIcon.Error)){
+                    form.AddButton(FormMessage.Ignore, ControlType.Cancel | ControlType.Focused);
+                        
+                    Button btnViewOptions = form.AddButton("View Options");
+                    btnViewOptions.Width += 16;
+                    btnViewOptions.Location = new Point(btnViewOptions.Location.X-16, btnViewOptions.Location.Y);
+
+                    if (form.ShowDialog() == DialogResult.OK && form.ClickedButton == btnViewOptions){
+                        browser.OpenSettings(typeof(TabSettingsSounds));
+                    }
+                }
+            });
         }
     }
 }
