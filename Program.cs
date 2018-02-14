d91b4bd1f33478d66e13f8b0657afa25bb1b4e63
chylex <contact@chylex.com>
2018-02-14 18:43:02 +0100

    Release 1.13.1

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace TweetDuck{
         public const string BrandName = "TweetDuck";
         public const string Website = "https://tweetduck.chylex.com";
 
-        public const string VersionTag = "1.13.0.1";
+        public const string VersionTag = "1.13.1";
 
         public static readonly bool IsPortable = File.Exists("makeportable");
 
