c311e24f086fa68ae4fe6db23ed049185fd46d49
chylex <contact@chylex.com>
2018-05-15 12:42:57 +0200

    Make it easier to update devtools file after updating CEF

--- Resources/PostCefUpdate.ps1
+++ Resources/PostCefUpdate.ps1
@@ -3,6 +3,11 @@ $ErrorActionPreference = "Stop"
 $MainProj = "..\TweetDuck.csproj"
 $BrowserProj = "..\subprocess\TweetDuck.Browser.csproj"
 
+$Match = Select-String -Path $MainProj '<Import Project="packages\\cef\.redist\.x86\.(.*?)\\'
+$Version = $Match.Matches[0].Groups[1].Value
+
+Copy-Item "..\packages\cef.redist.x86.${Version}\CEF\devtools_resources.pak" -Destination "..\bld\Resources\" -Force
+
 $Match = Select-String -Path $MainProj '<Import Project="packages\\CefSharp\.Common\.(.*?)\\'
 $Version = $Match.Matches[0].Groups[1].Value
 
