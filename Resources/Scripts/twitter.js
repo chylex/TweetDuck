cf7029037ee06ca070ef37521a474f88ab4ff43e
chylex <contact@chylex.com>
2020-05-16 18:02:37 +0200

    Fix twitter login page after it was migrated and broke detection

--- Resources/Scripts/twitter.js
+++ Resources/Scripts/twitter.js
@@ -1,7 +1,6 @@
 (function(){
   const isLogin = location.pathname === "/login";
   const isLogout = location.pathname === "/logout";
-  const isMobile = location.host === "mobile.twitter.com";
   
   //
   // Function: Inject custom CSS into the page.
@@ -23,13 +22,6 @@
     }
     else if (isLogout){
       document.documentElement.setAttribute("logout", "");
-    }
-    
-    if (isMobile){
-      document.documentElement.setAttribute("mobile", "");
-    }
-    else{
-      document.documentElement.setAttribute("desktop", "");
     }
   };
   
@@ -53,84 +45,57 @@
   //
   if (isLogin){
     document.addEventListener("DOMContentLoaded", function(){
-      if (isMobile){
-        triggerWhenExists("main h1", function(heading){
-          heading.parentNode.setAttribute("tweetduck-login-wrapper", "");
-          return true;
-        });
+      triggerWhenExists("main h1", function(heading){
+        heading.parentNode.setAttribute("tweetduck-login-wrapper", "");
+        return true;
+      });
+      
+      triggerWhenExists("a[href='/i/flow/signup']", function(texts){
+        texts = texts.parentNode;
         
-        triggerWhenExists("a[href='/i/flow/signup']", function(texts){
-          texts = texts.parentNode;
-          
-          let link = texts.childNodes[0];
-          let separator = texts.childNodes[1];
-          
-          if (link && separator){
-            texts.classList.add("tweetduck-login-links");
-            
-            link = link.cloneNode(false);
-            link.id = "tweetduck-helper";
-            link.href = "#";
-            link.innerText = "Import TweetDuck profile";
-            
-            texts.appendChild(separator.cloneNode(true));
-            texts.appendChild(link);
-            
-            link.addEventListener("click", function(){
-              $TD.openProfileImport();
-            });
-            
-            return true;
-          }
-          else{
-            return false;
-          }
-        });
-      }
-      else{
-        const openLinkExternally = function(e){
-          let href = e.currentTarget.getAttribute("href");
-          $TD.openBrowser(href[0] === '/' ? location.origin+href : href);
-          
-          e.preventDefault();
-          e.stopPropagation();
-        };
+        let link = texts.childNodes[0];
+        let separator = texts.childNodes[1];
         
-        for(let link of document.getElementsByTagName("A")){
-          link.addEventListener("click", openLinkExternally);
-        }
-        
-        let texts = document.querySelector(".page-canvas > div:last-child");
-        
-        if (texts){
-          texts.insertAdjacentHTML("beforeend", `<p id="tweetduck-helper">Used the TweetDuck app before? <a href="#">Import your profile »</a></p>`);
+        if (link && separator){
+          texts.classList.add("tweetduck-login-links");
           
-          texts.querySelector("#tweetduck-helper > a").addEventListener("click", function(){
+          link = link.cloneNode(false);
+          link.id = "tweetduck-helper";
+          link.href = "#";
+          link.innerText = "Import TweetDuck profile";
+          
+          texts.appendChild(separator.cloneNode(true));
+          texts.appendChild(link);
+          
+          link.addEventListener("click", function(){
             $TD.openProfileImport();
           });
+          
+          return true;
         }
-      }
+        else{
+          return false;
+        }
+      });
     });
   }
   
   //
   // Block: Hide cookie crap.
   //
-  if (isMobile){
-    document.addEventListener("DOMContentLoaded", function(){
-      triggerWhenExists("a[href^='https://help.twitter.com/rules-and-policies/twitter-cookies']", function(cookie){
-        while(!!cookie){
-          if (cookie.offsetHeight > 30){
-            cookie.remove();
-            return true;
-          }
-          else{
-            cookie = cookie.parentNode;
-          }
+  document.addEventListener("DOMContentLoaded", function(){
+    triggerWhenExists("a[href^='https://help.twitter.com/rules-and-policies/twitter-cookies']", function(cookie){
+      while(!!cookie){
+        if (cookie.offsetHeight > 30){
+          cookie.remove();
+          return true;
         }
-        
-        return false;
-      });
+        else{
+          cookie = cookie.parentNode;
+        }
+      }
+      
+      return false;
     });
-  }
+  });
 })();
