57111212ea98558b7935501cbdd9ecedc855b550
chylex <contact@chylex.com>
2023-07-17 12:49:43 +0200

    Fix warning message about TweetDeck Preview appearing on login screen

--- resources/Content/tweetdeck/tweetdeck_preview_warning.js
+++ resources/Content/tweetdeck/tweetdeck_preview_warning.js
@@ -1,7 +1,7 @@
 import { $TD } from "../api/bridge.js";
 
 export default function() {
-	if (!("TD" in window)) {
+	if (location.pathname === "/" && !("TD" in window)) {
 		$TD.alert("warning", "Some TweetDuck features failed to load. This might happen if your Twitter account is enrolled into the TweetDeck Preview, which TweetDuck does not support. Try opting out of the TweetDeck Preview to restore TweetDuck's functionality.");
 	}
 }
